c18a3176eb580c9fe77d35f96f472d1955601781
Sang Nguyen <tsang.2707@gmail.com>
2021-03-10 17:21:13 +0700

    update code

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,11 @@
+import { useHistory } from 'react-router-dom'
 import './index.scss'
 
 const Header = () => {
-
+    const history = useHistory()
     return (
         <div className="wrap-header">
-            <h1>Pokemon</h1>
+            <h1 onClick={() => history.push('/')}>Pokemon</h1>
         </div>
     )
 }
